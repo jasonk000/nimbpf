3e74fcebc43fdd4e52c48f01fdcc6ef111c076c5
Jason Koch <jkoch@netflix.com>
2019-10-21 11:20:50 -0700

    switch to nim logging module

--- src/nimbpf.nim
+++ src/nimbpf.nim
@@ -3,9 +3,12 @@ import posix
 import tables
 import options
 import parseutils
+import logging
 
 import nimbpf/uapibpf
 import nimbpf/libbpf
+
+var logger = newConsoleLogger()
 
 var mapFds = initTable[string, cint]()
 
@@ -27,7 +30,7 @@ proc getKernelVersion(): cuint =
     for c in uts.release:
       if c != '\x00':
         release = release & $(c)
-    # logInfo("uts.release=" & $release)
+    logger.log(lvlInfo, "uts.release=" & $release)
     let versiontag = release.split('-')[0].split('.')
     let major = parseUInt(versiontag[0])
     let minor = parseUInt(versiontag[1])
@@ -45,19 +48,19 @@ proc setRLimit(): void =
   r.rlim_max = 100*1024*1024 # 100mb
   let res = setrlimit(RLIMIT_MEMLOCK, r)
   if res == 0:
-    # logInfo("set rlimit: success")
+    logger.log(lvlInfo, "set rlimit: success")
   else:
-    # logWarning("could not set rlimit")
+    logger.log(lvlWarn, "could not set rlimit")
 
 proc checkKernelVersion(bpf_obj: ptr bpf_object): cuint =
   let kver: cuint = getKernelVersion()
-  # logInfo("kernel version detected = " & $kver)
+  logger.log(lvlInfo, "kernel version detected = " & $kver)
 
   let elfkver: cuint = bpf_object_kversion(bpf_obj)
   if kver == elfkver:
-    # logInfo("  bpf object kernel version = " & $elfkver & " (match)")
+    logger.log(lvlInfo, "  bpf object kernel version = " & $elfkver & " (match)")
   else:
-    # logWarning("  bpf object kernel version  = " & $elfkver & " (different, will overwrite)")
+    logger.log(lvlWarn, "  bpf object kernel version  = " & $elfkver & " (different, will overwrite)")
   return kver
 
 proc bootBpf*(elfBpfFile: cstring): void =
@@ -65,7 +68,7 @@ proc bootBpf*(elfBpfFile: cstring): void =
 
   var bpf_obj = bpf_object_open(elfBpfFile)
   var name = bpf_object_name(bpf_obj)
-  # logInfo("bpf object opened: " & $name)
+  logger.log(lvlInfo, "bpf object opened: " & $name)
 
   let kver = checkKernelVersion(bpf_obj)
   bpf_object_set_kversion(bpf_obj, kver)
@@ -83,56 +86,56 @@ proc bootBpf*(elfBpfFile: cstring): void =
     bpf_program_set_expected_attach_type(prg, MAX_BPF_ATTACH_TYPE)
 
   # do the load
-  # logInfo("bpf loading:")
+  logger.log(lvlInfo, "bpf loading:")
   let ret = bpf_object_load(bpf_obj)
   if ret == 0:
-    # logInfo("  bpf successfully loaded")
+    logger.log(lvlInfo, "  bpf successfully loaded")
   else:
-    # logErr("  bpf load failed: " & $ret) 
+    logger.log(lvlError, "  bpf load failed: " & $ret) 
 
   # open fds for discovered maps
-  # logInfo("loading maps:")
+  logger.log(lvlInfo, "loading maps:")
   var bpf_map = bpf_map_next(nil, bpf_obj)
   while bpf_map != nil:
     let title = $(bpf_map_name(bpf_map))
     let fd = bpf_map_fd(bpf_map)
     if fd == -1:
-      # logErr("  could not load map: " & title & "-" & $strerror(errno))
+      logger.log(lvlError, "  could not load map: " & title & "-" & $strerror(errno))
     else:
       mapFds[title] = fd
-      # logInfo("  loaded map: " & title)
+      logger.log(lvlInfo, "  loaded map: " & title)
     bpf_map = bpf_map_next(bpf_map, bpf_obj)
 
   # attach programs
-  # logInfo("attaching programs:")
+  logger.log(lvlInfo, "attaching programs:")
   for _, prg in bpf_programs:
     let rawtitle = $bpf_program_title(prg, false)
     let title = rawtitle.split('/')
     case title[0]:
       of "kprobe":
         discard bpf_program_attach_kprobe(prg, false, title[1].cstring)
-        # logInfo("  program: " & rawtitle & " -> attached")
+        logger.log(lvlInfo, "  program: " & rawtitle & " -> attached")
       of "kretprobe":
         discard bpf_program_attach_kprobe(prg, true, title[1].cstring)
-        # logInfo("  program: " & rawtitle & " -> attached")
+        logger.log(lvlInfo, "  program: " & rawtitle & " -> attached")
       else:
-        # logWarning("  program: " & rawtitle & " -> unsupported")
+        logger.log(lvlWarn, "  program: " & rawtitle & " -> unsupported")
 
 proc fetchFromContainerMap*(map: string, container: string): Option[uint64] =
   if not mapFds.hasKey(map):
-    # logErr("fetchFromContainerMap: count not find fd for map (was bpf loaded?): " & $map)
+    logger.log(lvlError, "fetchFromContainerMap: count not find fd for map (was bpf loaded?): " & $map)
     return none(uint64)
 
   var key = containerNameToPidNs(container)
   if key.isNone():
-    # logWarning("fetchFromContainerMap: could not find pidns for: " & $container)
+    logger.log(lvlWarn, "fetchFromContainerMap: could not find pidns for: " & $container)
     return none(uint64)
 
   var value: culonglong
   var fd = mapFds[map]
   let ret = bpf_map_lookup_elem(fd, addr(key), addr(value))
   if ret == -1:
-    # logWarning("fetchFromContainerMap: did not find value in map for: " & $container)
+    logger.log(lvlWarn, "fetchFromContainerMap: did not find value in map for: " & $container)
     return none(uint64)
 
   return some(value.uint64)
