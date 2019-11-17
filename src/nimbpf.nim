8ece00c6b65497ac8feb3b94f5371ff48adccdaa
Jason Koch <jkoch@netflix.com>
2019-11-17 11:20:02 -0800

    interim

--- src/nimbpf.nim
+++ src/nimbpf.nim
@@ -136,7 +136,7 @@ proc fetchFromMap*(map: string, key: var culong): Option[uint64] =
 
   return some(value.uint64)
 
-proc fetchFromMapPointerKey*(map: string, key: pointer): Option[uint64] =
+proc fetchFromMapPointerKey*(map: string, key: ptr any): Option[uint64] =
   if not mapFds.hasKey(map):
     logger.log(lvlError, "fetchFromMap: count not find fd for map (was bpf loaded?): " & $map)
     return none(uint64)
