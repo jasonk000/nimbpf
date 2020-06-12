5f316f778c7ef9fbd4d502bcc1787a48d0b513d6
Jason Koch <jkoch@netflix.com>
2020-06-12 09:34:17 -0700

    add identifiers

--- src/nimbpf/uapibpf.nim
+++ src/nimbpf/uapibpf.nim
@@ -22,15 +22,15 @@ cPlugin:
     sym.name = sym.name.strip(chars = {'_'})
     sym.name = sym.name.replace("__", "_")
 
-#cOverride:
+cOverride:
 #  const LIBBPF_ERRNO_LIBELF* = 4001
 #
-#  type u8 {.size:sizeof(int8).} = cuint
-#  type u32 {.size:sizeof(int32).} = cuint
-#  type u64 {.size:sizeof(int64).} = cuint
-#  type s16 {.size:sizeof(int16).} = cint
-#  type s32 {.size:sizeof(int32).} = cint
-#  type pid_t = cuint
+  type u8 {.size:sizeof(int8).} = cuint
+  type u32 {.size:sizeof(int32).} = cuint
+  type u64 {.size:sizeof(int64).} = cuint
+  type s16 {.size:sizeof(int16).} = cint
+  type s32 {.size:sizeof(int32).} = cint
+  type pid_t = cuint
 #
 #  type bpf_prog_info* {.importc: "struct bpf_prog_info", bycopy.} = object
 #    `type`*: u32
